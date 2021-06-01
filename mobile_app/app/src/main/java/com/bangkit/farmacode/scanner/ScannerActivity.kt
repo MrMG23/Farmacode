691634a831fd314d0adb9774175c59999add26b9
DevianWahyu <prof.devian.wahyu@gmail.com>
2021-06-01 20:39:26 +0700

    add scanner simulation not integrated with ML

--- mobile_app/app/src/main/java/com/bangkit/farmacode/scanner/ScannerActivity.kt
+++ mobile_app/app/src/main/java/com/bangkit/farmacode/scanner/ScannerActivity.kt
@@ -2,14 +2,25 @@ package com.bangkit.farmacode.scanner
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bangkit.farmacode.R
 import com.bangkit.farmacode.databinding.ActivityScannerBinding
 import com.bangkit.farmacode.drug.DrugActivity
 import com.bangkit.farmacode.patient.PatientActivity
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
 
 class ScannerActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityScannerBinding
+
+    // Scanner simulation
+    private lateinit var codeScanner: CodeScanner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,26 +28,81 @@ class ScannerActivity : AppCompatActivity() {
         binding = ActivityScannerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.scannerButton.setOnClickListener {
-            // Simulate the scanner to get the id
-            val id = binding.scannerInput.text.toString()
+//        binding.scannerButton.setOnClickListener {
+//            // Simulate the scanner to get the id
+//            val id = binding.scannerInput.text.toString()
+//
+//            // Intent check where to navigate
+//            when (id[0].toString()) {
+//                "0" -> {
+//                    Intent(this, PatientActivity::class.java).apply {
+//                        putExtra(PatientActivity.EXTRA_DATA, id)
+//                        startActivity(this)
+//                    }
+//                }
+//                "1" -> {
+//                    Intent(this, DrugActivity::class.java).apply {
+//                        putExtra(DrugActivity.EXTRA_DATA, id)
+//                        startActivity(this)
+//                    }
+//                }
+//            }
+//        }
 
-            // Intent check where to navigate
-            when (id[0].toString()) {
-                "0" -> {
-                    Intent(this, PatientActivity::class.java).apply {
-                        putExtra(PatientActivity.EXTRA_DATA, id)
-                        startActivity(this)
+
+        // Scanner simulation
+        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+
+        codeScanner = CodeScanner(this, scannerView)
+
+        // Parameters (default values)
+        codeScanner.camera = CodeScanner.CAMERA_BACK
+        codeScanner.formats = CodeScanner.ALL_FORMATS
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE
+        codeScanner.scanMode = ScanMode.SINGLE
+        codeScanner.isAutoFocusEnabled = true
+        codeScanner.isFlashEnabled = false
+
+        // Callbacks
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                when (it.text[0].toString()) {
+                    "0" -> {
+                        Intent(this, PatientActivity::class.java).apply {
+                            putExtra(PatientActivity.EXTRA_DATA, it.text)
+                            startActivity(this)
+                        }
                     }
-                }
-                "1" -> {
-                    Intent(this, DrugActivity::class.java).apply {
-                        putExtra(DrugActivity.EXTRA_DATA, id)
-                        startActivity(this)
+                    "1" -> {
+                        Intent(this, DrugActivity::class.java).apply {
+                            putExtra(DrugActivity.EXTRA_DATA, it.text)
+                            startActivity(this)
+                        }
                     }
                 }
             }
         }
+        codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
+            runOnUiThread {
+                Toast.makeText(
+                    this, "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
 
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
     }
 }
